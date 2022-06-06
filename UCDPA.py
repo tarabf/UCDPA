f404c147a8a0ca25ab79fa9604b2719dc9c4a27c
Tara Burke <tara.burke@flutter.com>
2022-06-06 16:12:23 +0100

    Project 1

---
+++ UCDPA.py
@@ -0,0 +1,47 @@
+import pandas as pd
+cso = pd.read_csv(r'C:\Users\tarab_000\Documents\Data Analytics CSV\RSM05.20220603T150653.csv')
+print(cso)
+
+print(cso[['Month','UNIT']])
+
+print(cso[0:5][['Month','UNIT']])
+
+print(cso.iloc[[1,10],[1,2]])
+
+print(cso.iloc[0:5,0:4])
+
+print(cso[cso['VALUE']>181.5])
+print(cso.drop_duplicates(subset=['Month']).head())
+
+print(cso.isnull())
+
+print(cso.isnull())
+
+print(cso.shape[0])
+
+value_mean = cso['VALUE'].mean()
+print(value_mean)
+
+value_median = cso['VALUE'].median()
+print(value_median)
+
+print(cso.groupby('NACE Group').mean())
+
+print(cso.groupby('NACE Group').agg(['mean','median']))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
